41d7af34f25c73dde0ad29b5862105646f2199ff
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-26 23:24:10 -0300

    creating input that filters by title

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    padding: 10px;
+    width: 300px;
+    background-color: transparent;
+    border: 1px solid #c400fa;
+    border-radius: 8px;
+    font-size: 20px;
+    color: #c400fa;
+`;
+
+export default Input
