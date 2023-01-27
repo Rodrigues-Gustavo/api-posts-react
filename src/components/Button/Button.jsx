4aa389db10c1244847c65b644a8e346687168605
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-26 23:18:56 -0300

    creating load more button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    padding: 10px;
+    width: 300px;
+    cursor: pointer;
+    background-color: transparent;
+    border: 1px solid;
+    font-size: 22px;
+    border: 8px;
+
+    &:hover { background-color: #f3f3f3 }
+`;
+
+export default Button
